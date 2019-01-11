e88762edb183f235b9f5a647c77b85903b76cd70
Krzysztof Zbudniewek <krzysztof.zbudniewek@gmail.com>
2019-01-11 20:38:55 +0100

    Check for EXPANDED_CODE_SIGN_IDENTITY_NAME first

--- LaunchAtLogin/copy-helper.sh
+++ LaunchAtLogin/copy-helper.sh
@@ -9,7 +9,12 @@ mkdir -p "$helper_dir"
 cp -rf "$origin_helper_path" "$helper_dir/"
 
 defaults write "$helper_path/Contents/Info" CFBundleIdentifier -string "$PRODUCT_BUNDLE_IDENTIFIER-LaunchAtLoginHelper"
-codesign --force --sign="$EXPANDED_CODE_SIGN_IDENTITY_NAME" "$helper_path"
+if [[ -z "$EXPANDED_CODE_SIGN_IDENTITY_NAME" ]]; then
+	echo "EXPANDED_CODE_SIGN_IDENTITY_NAME: empty"
+else
+	echo "EXPANDED_CODE_SIGN_IDENTITY_NAME: $EXPANDED_CODE_SIGN_IDENTITY_NAME"
+	codesign --force --sign="$EXPANDED_CODE_SIGN_IDENTITY_NAME" "$helper_path"
+fi
 
 if [[ $CONFIGURATION == "Release" ]]; then
 	rm -rf "$origin_helper_path"
